879693c41077052faf6f57659ba9b73803ba40f4
Lucas Zarandón <86167490+lucaszhh@users.noreply.github.com>
2023-06-14 13:12:02 -0300

    subiendo clase 4 de ia con python
    al principio parecía algo difícil pero ya logre pensar en una posible solución jeje

---
+++ 4_mazeRunner/README.txt
@@ -0,0 +1,1 @@
+## Comence la busqueda para lograr completar el laberinto .... coming soon va a funcionar xd (:
---
+++ 4_mazeRunner/index.py
@@ -0,0 +1,8 @@
+from entity import Maze, Node
+
+laberinto = Maze("./mazes/maze.txt")
+laberinto1 = Maze("./mazes/maze1.txt")
+
+laberinto.currentState
+laberinto.initialState
+
