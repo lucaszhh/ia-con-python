e04322ac69247250e14cae4e6af310d0fce7ee8b
lucaszhh <lucaszarandon24@gmail.com>
2023-07-16 23:19:08 -0300

    add pygame to implement draw maze runer this is awesome

--- 6_mazeRunner3/app.py
+++ 6_mazeRunner3/app.py
@@ -1,4 +1,21 @@
-from entities import *
+import pygame, sys
+from entities import Laberinto
+
+""" Paleta de colores """
+BLANCO = (255,255,255)
+NEGRO = (0,0,0)
+VERDE1 = (55,148,73)
+GRIS = (22,22,22)
+
+""" Inizializador de pantalla y de pygame"""
+pygame.init()
+PANTALLA =  pygame.display.set_mode((1000,500))
+pygame.display.set_caption("MAZE") #nombre de la pantalla
+PANTALLA.fill(BLANCO) # seteo del color de la pantalla
+
+""" Perzonalizacion del icono de la pantalla """
+ICON = pygame.image.load("./images/maze.png")
+pygame.display.set_icon(ICON)
 
 """ import laberintos """
 laberintoTxt = "./laberintos/laberinto.txt"
@@ -12,3 +29,33 @@ Laberinto("BFS", laberintoTxt3).resolver()
 Laberinto("DFS", laberintoTxt3).resolver()
 Laberinto("GBFS", laberintoTxt3).resolver()
 Laberinto("A*", laberintoTxt3).resolver()
+
+""" Funciones de dibujo """
+
+def DIBUJAR_CUADRADO (x,y,color):
+    pygame.draw.rect(PANTALLA, color,(x,y,20,20))
+
+def DIBUJAR_META (nodos_solucion):
+    pygame.draw.polygon(PANTALLA, VERDE2,  nodos_solucion, 5)
+
+def DIBUJAR_FILA(y,filas):
+    for cuadrado in filas:
+        POSICIONX = 20
+        if cuadrado == "#":
+            DIBUJAR_CUADRADO(POSICIONX, y, NEGRO )
+        elif cuadrado == "M":
+            DIBUJAR_CUADRADO(POSICIONX, y, VERDE1 )
+        elif cuadrado == "I":
+            DIBUJAR_CUADRADO(POSICIONX, y, VERDE1 )
+        else:
+            DIBUJAR_CUADRADO(POSICIONX, y, GRIS)
+        POSICIONX += 20
+
+
+""" Bucle que permite que la página no se cierre """
+while True:
+    for event in pygame.event.get():
+        if event.type == quit:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update() #funcion que permite que se actualice la pantalla 
--- 6_mazeRunner3/entities.py
+++ 6_mazeRunner3/entities.py
@@ -1,3 +1,49 @@
+import pygame
+import sys
+import time
+
+""" Paleta de colores """
+BLANCO = (255,255,255)
+NEGRO = (0,0,0)
+VERDE1 = (55,148,73)
+GRIS = (22,22,22)
+
+""" Inizializador de pantalla y de pygame"""
+pygame.init()
+PANTALLA =  pygame.display.set_mode((1000,500))
+pygame.display.set_caption("MAZE") #nombre de la pantalla
+PANTALLA.fill(BLANCO) # seteo del color de la pantalla
+
+""" Perzonalizacion del icono de la pantalla """
+ICON = pygame.image.load("./images/maze.png")
+pygame.display.set_icon(ICON)
+
+
+""" Funciones de dibujo """
+
+def DIBUJAR_CUADRADO(x, y, color):
+    pygame.draw.rect(PANTALLA, NEGRO, (x, y, 20, 20))
+    pygame.draw.rect(PANTALLA, color, (x, y, 18, 18))
+
+
+def DIBUJAR_META (nodos_solucion):
+    pygame.draw.polygon(PANTALLA, VERDE1,  nodos_solucion, 5)
+
+def DIBUJAR_FILA(y,filas):
+    POSICIONX = 20
+    for cuadrado in filas:
+        if cuadrado == "#":
+            DIBUJAR_CUADRADO(POSICIONX, y, NEGRO )
+        elif cuadrado == "M":
+            DIBUJAR_CUADRADO(POSICIONX, y, VERDE1 )
+        elif cuadrado == "I":
+            DIBUJAR_CUADRADO(POSICIONX, y, VERDE1 )
+        elif cuadrado == " ":
+            DIBUJAR_CUADRADO(POSICIONX, y, GRIS)
+        POSICIONX += 20
+
+
+
 class Nodo():
     def __init__(self, _estado, _padre, distancia):
         self.estado = _estado
@@ -46,7 +92,7 @@ class Laberinto():
                 if self.laberinto[fila][columna]==' ': 
                     fila_paredes.append(False) 
                 elif self.laberinto[fila][columna]=='I':   
-                    self.inicio=(fila,columna)         
+                    self.inicio=(fila,columna)  # Mover esta línea fuera del bucle         
                     fila_paredes.append(False)
                 elif self.laberinto[fila][columna]=='M':   
                     self.meta=(fila,columna)
@@ -56,52 +102,24 @@ class Laberinto():
             self.paredes.append(fila_paredes)
 
     def tomar_distancia(self,estado):
-            x1, y1 = estado
-            x2, y2 = self.meta
-            dixt_y = abs(y2 - y1)
-            dixt_x = abs(x2 - x1)
-            return dixt_x + dixt_y
+        x1, y1 = estado
+        x2, y2 = self.meta
+        dixt_y = abs(y2 - y1)
+        dixt_x = abs(x2 - x1)
+        return dixt_x + dixt_y
     
     def dibujar_solucion(self):
-        print("# Paredes \nI Inicio \nM Meta \n+ Solucion")
-        print("# Laberinto sin resolver")
-        laberinto_str = ""
         for fila in range(self.alto):
             filas = []
             for columna in range(self.ancho):
                 filas.append(self.laberinto[fila][columna])
-            laberinto_str += "".join(filas) + "\n"
-        print(laberinto_str)
-        
-        print("# Laberinto resuelto")
-        laberinto_str = ""
-        for fila in range(self.alto):
-            filas = []
-            for columna in range(self.ancho):
-                if self.laberinto[fila][columna] == '#':
-                    filas.append("#")
-                elif self.laberinto[fila][columna] == 'I':
-                    filas.append("I")
-                elif self.laberinto[fila][columna] == 'M':
-                    filas.append("M")
-                elif self.laberinto[fila][columna] == ' ':
-                    filas.append(" ")
-                
-                nodo_actual = None
-                for nodo in self.solucion:
-                    if nodo.estado == (fila, columna):
-                        nodo_actual = nodo
-                        break
+            DIBUJAR_FILA((fila + 1) * 20, filas)
+            print(filas, "filas")
+        for nodo in self.solucion:
+            time.sleep(0.2)
+            DIBUJAR_CUADRADO((nodo.estado[1] * 20)+20, (nodo.estado[0] + 1) * 20, VERDE1)
+            pygame.display.update()
 
-                if nodo_actual and nodo_actual.estado != self.inicio:
-                    filas[columna] = "+"
-
-            laberinto_str += "".join(filas) + "\n"
-        print(laberinto_str)
-        print(f"Laberinto resuelto por el algoritmo {self.algoritmo}")
-        print("**************************************************")
-
-    
     def expandir_nodo(self,_nodo):
         fila, columna = _nodo.estado
         vecinos = []
@@ -126,13 +144,13 @@ class Laberinto():
 
         while True:
             if frontera.esta_vacia():
-                print(f"El algoritmo {self.algoritmo} no encontro la solucion")
+                print(f"El algoritmo {self.algoritmo} no encontró la solución")
                 return
 
             nodo_actual = frontera.quitar_nodo()
 
             if nodo_actual.estado == self.meta:
-                print(f"El algoritmo {self.algoritmo} encontro la meta con un total de {len(self.explorados)} nodos explorados")
+                print(f"El algoritmo {self.algoritmo} encontró la meta con un total de {len(self.explorados)} nodos explorados")
                 while nodo_actual.padre is not None:
                     self.solucion.append(nodo_actual.padre)
                     nodo_actual = nodo_actual.padre
@@ -157,3 +175,25 @@ class Laberinto():
                         nodo_hijo = Nodo(vecino, nodo_actual, costo)
 
                     frontera.agregar_nodo(nodo_hijo)
+
+
+""" import laberintos """
+laberintoTxt = "./laberintos/laberinto.txt"
+laberintoTxt1 = "./laberintos/laberinto1.txt"
+laberintoTxt2 = "./laberintos/laberinto2.txt"
+laberintoTxt3 = "./laberintos/laberinto3.txt"
+
+""" Posibles algoritmos BFS DFS GBFS A* """
+
+Laberinto("GBFS", laberintoTxt3).resolver()
+
+
+""" Bucle que permite que la página no se cierre """
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
+               
